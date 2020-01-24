0ef01f01c71f301555202e4564ff614a0e3f26ba
codefuldom <dom@codefuldom.com>
2020-01-23 23:58:59 -0600

    :rocket: add struct to project

--- structs/main.go
+++ structs/main.go
@@ -1,1 +1,26 @@
-// Structs - short for structure
+// Struct - short for structure
+package main
+
+import "fmt"
+
+type contactInfo struct {
+  email string
+  zipCode int
+}
+type person struct {
+  firstName  string
+  lastName string
+  contact contactInfo
+}
+
+func main() {
+  jim := person{
+    firstName: "Meg",
+    lastName: "Parsons",
+    contact: contactInfo{
+      email: "jim@gmail.com",
+      zipCode: 63109,
+    },
+  }
+  fmt.Printf("%+v", jim)
+}
